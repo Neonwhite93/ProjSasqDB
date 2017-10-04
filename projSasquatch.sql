5b8bdea3f74bc9bcc751f7d88ce83d7096cde7e2
Donovan Miller <dsm425@humboldt.edu>
2017-10-04 14:43:12 -0700

    Updated sql scripts

--- projSasquatch.sql
+++ projSasquatch.sql
@@ -1,71 +1,89 @@
--- Project Sasquatch
--- Donovan Miller, 8/11/2017
--- Edited by: Donovan Miller 9/6/17
-
--- Drops/removes the tables.
-Drop Table Rep_in_set cascade constraints;
-
-DROP TABLE Part_of_body cascade constraints;
-
-DROP TABLE The_set cascade constraints;
-
-DROP TABLE Exercise cascade constraints;
-
-DROP TABLE User cascade constraints;
-
--- Exercise table contains the name, type
--- and difficulty level of the exercise.
--- Exercise types: warmups, push, pull, legs, core
--- Difficulty level:  Easy, Intermediate , Hard
-
-CREATE TABLE Exercise
-(exrcise_id     INT  NOT NULL,
- exrcise_name   VARCHAR(20),
- exrcise_type   VARCHAR(20),
- exrcise_lvl    VARCHAR(10), 
- PRIMARY KEY    (exrcise_id)
-);
-
--- The_set table contains the number of sets
--- and is meant to connect to the Rep_in_set table
--- easier. 
-CREATE TABLE The_set
-(set_id       INT NOT NULL,
- exrcise_id   INT NOT NULL,
- num_of_set   INT,
- PRIMARY KEY  (set_id),
- FOREIGN KEY  (exrcise_id) references Exercise
-);
-
--- Rep_in_set keeps track of the reps and weight w/in the set
--- using the num_of_set to reference the The_set table
--- reps, weight, and perceived_exertion are multivalued attributes
--- the The_set table
-CREATE TABLE Rep_in_set
-(rep_id             INT,
- set_id             INT,
- reps               INT,
- weight             INT,
- perceived_exertion INT,
- PRIMARY KEY        (rep_id),
- FOREIGN KEY        (set_id) references The_set
-);
-
--- Part_of_body keeps track of the parts of the body
--- The part_name is a multivalued attribute for 
--- the Exercise table
-CREATE TABLE Part_of_body
-(part_id        INT,
- exrcise_id     INT,
- part_name      VARCHAR(30),
- PRIMARY KEY    (part_id),
- FOREIGN KEY    (exrcise_id) references Exercise
-);
+-- -----------------------------------------------------
+-- Table Exercise
+-- -----------------------------------------------------
+CREATE TABLE Exercise (
+  exrcise_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  exrcise_name VARCHAR(30) NOT NULL,
+  exrcise_type ENUM('WARMUP', 'PUSH', 'PULL', 'LEGS', 'CORE') NOT NULL,
+  exrcise_lvl ENUM('EASY', 'INTERMEDIATE', 'HARD') NOT NULL,
+  PRIMARY KEY (exrcise_id));
 
 
-CREATE TABLE User
-(user_id     INT NOT NULL,
- username    VARCHAR(45) NOT NULL,
- password    VARCHAR(45) NOT NULL,
- PRIMARY KEY (user_id)
-);
+-- -----------------------------------------------------
+-- Table Set_Group
+-- -----------------------------------------------------
+CREATE TABLE Set_Group (
+  set_group_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  num_of_sets INT UNSIGNED,
+  exercise_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY (set_group_id),
+  FOREIGN KEY (exrcise_id) REFERENCES Exercise (exrcise_id)
+  );
+
+
+-- -----------------------------------------------------
+-- Table The_Set
+-- -----------------------------------------------------
+CREATE TABLE The_Set (
+  set_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  reps_fin INT UNSIGNED,
+  weight_used INT UNSIGNED,
+  perceived_exert INT UNSIGNED,
+  PRIMARY KEY (set_id),
+  FOREIGN KEY (set_group_id) REFERENCES Set_Group (set_group_id)
+  );
+
+
+
+-- -----------------------------------------------------
+-- Table User
+-- -----------------------------------------------------
+CREATE TABLE User (
+  username VARCHAR(8) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  type ENUM('STANDARD', 'ADMIN') NOT NULL,
+  class ENUM('PE 157', 'PE 158', 'NONE') NOT NULL,
+  team ENUM('FOOTBALL', 'SOFTBALL', 'BASKETBALL', 'CROSS COUNTRY', 'TRACK AND FIELD', 'SOCCER', 'ROWING', 'VOLLEYBALL', 'NONE') NOT NULL,
+  f_name VARCHAR(30) NOT NULL,
+  l_name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (username)
+  );
+
+
+-- -----------------------------------------------------
+-- Table Workout
+-- -----------------------------------------------------
+CREATE TABLE Workout (
+  workout_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  workout_name VARCHAR(45) NOT NULL,
+  assigned_bool BOOLEAN NOT NULL,
+  username VARCHAR(8) NOT NULL,
+  PRIMARY KEY (workout_id),
+  FOREIGN KEY (username) REFERENCES User (username)
+    );
+
+
+-- -----------------------------------------------------
+-- Table Completed_Workout
+-- -----------------------------------------------------
+CREATE TABLE Completed_Workout (
+  workout_id INT UNSIGNED NOT NULL,
+  username VARCHAR(8) NOT NULL,
+  date DATE NOT NULL,
+  PRIMARY KEY (workout_id),
+    FOREIGN KEY (workout_id) REFERENCES Workout (workout_id),
+    FOREIGN KEY (username) REFERENCES Users (username)
+    );
+
+
+-- -----------------------------------------------------
+-- Table Set_Group_In_Workout
+-- -----------------------------------------------------
+CREATE TABLE Set_Group_In_Workout (
+  set_group_id INT UNSIGNED NOT NULL,
+  workout_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY (set_group_id, workout_id),
+  FOREIGN KEY (set_group_id) REFERENCES Set_Group (set_group_id),
+  FOREIGN KEY (workout_id) REFERENCES Workout (workout_id)
+  );
+
